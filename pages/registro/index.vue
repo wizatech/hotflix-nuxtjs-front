5ba5a3f0fb1b810f75a01602716c452d3a1167be
wizatek <diogown3d@gmail.com>
2020-05-09 21:58:08 -0400

    create page register users

---
+++ pages/registro/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="sign section--bg" data-bg="/_nuxt/assets/img/section.jpg" style="background: url('/_nuxt/assets/img/section.jpg') center center / cover no-repeat;">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="sign__content">
+            <!-- registration form -->
+            <form  @submit.prevent="register()" class="sign__form">
+              <a href="/" class="sign__logo">
+                <img src="~assets/img/logo.png" alt="">
+                <img src="" alt="">
+              </a>
+
+              <div class="sign__group">
+                <input type="text" class="sign__input" v-model="fields.username" placeholder="Nome">
+              </div>
+
+              <div class="sign__group">
+                <input type="text" class="sign__input" v-model="fields.email" placeholder="Email">
+              </div>
+
+              <div class="sign__group">
+                <input type="password" class="sign__input" v-model="fields.password" placeholder="Senha">
+              </div>
+
+              <button class="sign__btn" type="submit">Cadastrar</button>
+
+              <span class="sign__text">Já possui uma conta? <nuxt-link to="/">clique aqui!</nuxt-link></span>
+            </form>
+            <!-- registration form -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      fields: {
+        username: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async register () {
+      const fields = (await this.$axios.post('users', this.fields)).data
+      if (fields) {
+        this.$router.push('/')
+      }
+    }
+  }
+}
+</script>
